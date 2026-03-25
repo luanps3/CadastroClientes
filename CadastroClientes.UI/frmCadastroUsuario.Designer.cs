00a3928175f2aa823b5590e478ee3dfaf8bc3966
luan.coesposito <luan.coesposito@SMP0581321W10-1>
2026-03-24 22:25:13 -0300

    Refatora frmCadastroUsuario e integra com AutenticacaoService
    - Refatoração completa do formulário de cadastro de usuário
    - Injeção de AutenticacaoService e cadastro assíncrono
    - Substituição dos botões antigos por btnCadastrar e btnCancelar
    - Adição de lblMensagem para feedback ao usuário
    - Validação de campos e exibição centralizada de erros
    - Ajustes visuais e melhorias de usabilidade
    - Remoção de código e usings desnecessários

--- CadastroClientes.UI/frmCadastroUsuario.Designer.cs
+++ CadastroClientes.UI/frmCadastroUsuario.Designer.cs
@@ -32,47 +32,49 @@
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges11 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges12 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges11 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges12 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges13 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges14 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            btnCancelar = new Guna.UI2.WinForms.Guna2Button();
             guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
             guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
             groupBox1 = new GroupBox();
-            txtNome = new Guna.UI2.WinForms.Guna2TextBox();
-            txtLogin = new Guna.UI2.WinForms.Guna2TextBox();
-            txtSenha = new Guna.UI2.WinForms.Guna2TextBox();
-            txtConfirmarSenha = new Guna.UI2.WinForms.Guna2TextBox();
             chkAtivo = new Guna.UI2.WinForms.Guna2CheckBox();
-            guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
+            txtConfirmarSenha = new Guna.UI2.WinForms.Guna2TextBox();
+            txtSenha = new Guna.UI2.WinForms.Guna2TextBox();
+            txtLogin = new Guna.UI2.WinForms.Guna2TextBox();
+            txtNome = new Guna.UI2.WinForms.Guna2TextBox();
+            btnCadastrar = new Guna.UI2.WinForms.Guna2Button();
+            lblMensagem = new Guna.UI2.WinForms.Guna2HtmlLabel();
             guna2Panel1.SuspendLayout();
             groupBox1.SuspendLayout();
             SuspendLayout();
             // 
-            // guna2Button1
+            // btnCancelar
             // 
-            guna2Button1.BorderRadius = 5;
-            guna2Button1.CustomizableEdges = customizableEdges1;
-            guna2Button1.DisabledState.BorderColor = Color.DarkGray;
-            guna2Button1.DisabledState.CustomBorderColor = Color.DarkGray;
-            guna2Button1.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
-            guna2Button1.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
-            guna2Button1.FillColor = Color.Maroon;
-            guna2Button1.Font = new Font("Century Gothic", 7.8F);
-            guna2Button1.ForeColor = Color.White;
-            guna2Button1.Location = new Point(174, 328);
-            guna2Button1.Name = "guna2Button1";
-            guna2Button1.ShadowDecoration.CustomizableEdges = customizableEdges2;
-            guna2Button1.Size = new Size(99, 37);
-            guna2Button1.TabIndex = 0;
-            guna2Button1.Text = "Cancelar";
+            btnCancelar.BorderRadius = 5;
+            btnCancelar.CustomizableEdges = customizableEdges1;
+            btnCancelar.DisabledState.BorderColor = Color.DarkGray;
+            btnCancelar.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnCancelar.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnCancelar.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnCancelar.FillColor = Color.Maroon;
+            btnCancelar.Font = new Font("Century Gothic", 7.8F, FontStyle.Bold);
+            btnCancelar.ForeColor = Color.White;
+            btnCancelar.Location = new Point(174, 336);
+            btnCancelar.Name = "btnCancelar";
+            btnCancelar.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            btnCancelar.Size = new Size(99, 37);
+            btnCancelar.TabIndex = 0;
+            btnCancelar.Text = "Cancelar";
+            btnCancelar.Click += btnCancelar_Click;
             // 
             // guna2Panel1
             // 
@@ -104,7 +106,7 @@
             groupBox1.Controls.Add(txtSenha);
             groupBox1.Controls.Add(txtLogin);
             groupBox1.Controls.Add(txtNome);
-            groupBox1.Font = new Font("Century Gothic", 7.8F);
+            groupBox1.Font = new Font("Century Gothic", 7.8F, FontStyle.Bold);
             groupBox1.ForeColor = Color.FromArgb(94, 148, 255);
             groupBox1.Location = new Point(12, 94);
             groupBox1.Name = "groupBox1";
@@ -113,68 +115,24 @@
             groupBox1.TabStop = false;
             groupBox1.Text = "Informações do Usuário";
             // 
-            // txtNome
+            // chkAtivo
             // 
-            txtNome.BorderRadius = 5;
-            txtNome.CustomizableEdges = customizableEdges11;
-            txtNome.DefaultText = "";
-            txtNome.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
-            txtNome.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
-            txtNome.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
-            txtNome.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
-            txtNome.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
-            txtNome.Font = new Font("Segoe UI", 9F);
-            txtNome.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
-            txtNome.Location = new Point(6, 27);
-            txtNome.Margin = new Padding(3, 4, 3, 4);
-            txtNome.Name = "txtNome";
-            txtNome.PlaceholderText = "Nome";
-            txtNome.SelectedText = "";
-            txtNome.ShadowDecoration.CustomizableEdges = customizableEdges12;
-            txtNome.Size = new Size(255, 30);
-            txtNome.TabIndex = 0;
-            // 
-            // txtLogin
-            // 
-            txtLogin.BorderRadius = 5;
-            txtLogin.CustomizableEdges = customizableEdges9;
-            txtLogin.DefaultText = "";
-            txtLogin.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
-            txtLogin.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
-            txtLogin.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
-            txtLogin.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
-            txtLogin.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
-            txtLogin.Font = new Font("Segoe UI", 9F);
-            txtLogin.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
-            txtLogin.Location = new Point(6, 65);
-            txtLogin.Margin = new Padding(3, 4, 3, 4);
-            txtLogin.Name = "txtLogin";
-            txtLogin.PlaceholderText = "Login";
-            txtLogin.SelectedText = "";
-            txtLogin.ShadowDecoration.CustomizableEdges = customizableEdges10;
-            txtLogin.Size = new Size(255, 30);
-            txtLogin.TabIndex = 0;
-            // 
-            // txtSenha
-            // 
-            txtSenha.BorderRadius = 5;
-            txtSenha.CustomizableEdges = customizableEdges7;
-            txtSenha.DefaultText = "";
-            txtSenha.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
-            txtSenha.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
-            txtSenha.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
-            txtSenha.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
-            txtSenha.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
-            txtSenha.Font = new Font("Segoe UI", 9F);
-            txtSenha.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
-            txtSenha.Location = new Point(6, 103);
-            txtSenha.Margin = new Padding(3, 4, 3, 4);
-            txtSenha.Name = "txtSenha";
-            txtSenha.PlaceholderText = "Senha";
-            txtSenha.SelectedText = "";
-            txtSenha.ShadowDecoration.CustomizableEdges = customizableEdges8;
-            txtSenha.Size = new Size(255, 30);
-            txtSenha.TabIndex = 0;
+            chkAtivo.AutoSize = true;
+            chkAtivo.Checked = true;
+            chkAtivo.CheckedState.BorderColor = Color.FromArgb(94, 148, 255);
+            chkAtivo.CheckedState.BorderRadius = 0;
+            chkAtivo.CheckedState.BorderThickness = 0;
+            chkAtivo.CheckedState.FillColor = Color.FromArgb(94, 148, 255);
+            chkAtivo.CheckState = CheckState.Checked;
+            chkAtivo.Location = new Point(6, 178);
+            chkAtivo.Name = "chkAtivo";
+            chkAtivo.Size = new Size(114, 20);
+            chkAtivo.TabIndex = 1;
+            chkAtivo.Text = "Usuário Ativo";
+            chkAtivo.UncheckedState.BorderColor = Color.FromArgb(125, 137, 149);
+            chkAtivo.UncheckedState.BorderRadius = 0;
+            chkAtivo.UncheckedState.BorderThickness = 0;
+            chkAtivo.UncheckedState.FillColor = Color.FromArgb(125, 137, 149);
             // 
             // txtConfirmarSenha
             // 
@@ -197,65 +155,122 @@
             txtConfirmarSenha.Size = new Size(255, 30);
             txtConfirmarSenha.TabIndex = 0;
             // 
-            // chkAtivo
+            // txtSenha
             // 
-            chkAtivo.AutoSize = true;
-            chkAtivo.Checked = true;
-            chkAtivo.CheckedState.BorderColor = Color.FromArgb(94, 148, 255);
-            chkAtivo.CheckedState.BorderRadius = 0;
-            chkAtivo.CheckedState.BorderThickness = 0;
-            chkAtivo.CheckedState.FillColor = Color.FromArgb(94, 148, 255);
-            chkAtivo.CheckState = CheckState.Checked;
-            chkAtivo.Location = new Point(6, 178);
-            chkAtivo.Name = "chkAtivo";
-            chkAtivo.Size = new Size(114, 21);
-            chkAtivo.TabIndex = 1;
-            chkAtivo.Text = "Usuário Ativo";
-            chkAtivo.UncheckedState.BorderColor = Color.FromArgb(125, 137, 149);
-            chkAtivo.UncheckedState.BorderRadius = 0;
-            chkAtivo.UncheckedState.BorderThickness = 0;
-            chkAtivo.UncheckedState.FillColor = Color.FromArgb(125, 137, 149);
+            txtSenha.BorderRadius = 5;
+            txtSenha.CustomizableEdges = customizableEdges7;
+            txtSenha.DefaultText = "";
+            txtSenha.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtSenha.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtSenha.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtSenha.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtSenha.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtSenha.Font = new Font("Segoe UI", 9F);
+            txtSenha.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtSenha.Location = new Point(6, 103);
+            txtSenha.Margin = new Padding(3, 4, 3, 4);
+            txtSenha.Name = "txtSenha";
+            txtSenha.PlaceholderText = "Senha";
+            txtSenha.SelectedText = "";
+            txtSenha.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            txtSenha.Size = new Size(255, 30);
+            txtSenha.TabIndex = 0;
             // 
-            // guna2Button2
+            // txtLogin
             // 
-            guna2Button2.BorderRadius = 5;
-            guna2Button2.CustomizableEdges = customizableEdges13;
-            guna2Button2.DisabledState.BorderColor = Color.DarkGray;
-            guna2Button2.DisabledState.CustomBorderColor = Color.DarkGray;
-            guna2Button2.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
-            guna2Button2.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
-            guna2Button2.FillColor = Color.Green;
-            guna2Button2.Font = new Font("Century Gothic", 7.8F);
-            guna2Button2.ForeColor = Color.White;
-            guna2Button2.Location = new Point(27, 328);
-            guna2Button2.Name = "guna2Button2";
-            guna2Button2.ShadowDecoration.CustomizableEdges = customizableEdges14;
-            guna2Button2.Size = new Size(99, 37);
-            guna2Button2.TabIndex = 0;
-            guna2Button2.Text = "Cadastrar";
+            txtLogin.BorderRadius = 5;
+            txtLogin.CustomizableEdges = customizableEdges9;
+            txtLogin.DefaultText = "";
+            txtLogin.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtLogin.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtLogin.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtLogin.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtLogin.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtLogin.Font = new Font("Segoe UI", 9F);
+            txtLogin.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtLogin.Location = new Point(6, 65);
+            txtLogin.Margin = new Padding(3, 4, 3, 4);
+            txtLogin.Name = "txtLogin";
+            txtLogin.PlaceholderText = "Login";
+            txtLogin.SelectedText = "";
+            txtLogin.ShadowDecoration.CustomizableEdges = customizableEdges10;
+            txtLogin.Size = new Size(255, 30);
+            txtLogin.TabIndex = 0;
+            // 
+            // txtNome
+            // 
+            txtNome.BorderRadius = 5;
+            txtNome.CustomizableEdges = customizableEdges11;
+            txtNome.DefaultText = "";
+            txtNome.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtNome.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtNome.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtNome.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtNome.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtNome.Font = new Font("Segoe UI", 9F);
+            txtNome.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtNome.Location = new Point(6, 27);
+            txtNome.Margin = new Padding(3, 4, 3, 4);
+            txtNome.Name = "txtNome";
+            txtNome.PlaceholderText = "Nome";
+            txtNome.SelectedText = "";
+            txtNome.ShadowDecoration.CustomizableEdges = customizableEdges12;
+            txtNome.Size = new Size(255, 30);
+            txtNome.TabIndex = 0;
+            // 
+            // btnCadastrar
+            // 
+            btnCadastrar.BorderRadius = 5;
+            btnCadastrar.CustomizableEdges = customizableEdges13;
+            btnCadastrar.DisabledState.BorderColor = Color.DarkGray;
+            btnCadastrar.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnCadastrar.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnCadastrar.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnCadastrar.FillColor = Color.Green;
+            btnCadastrar.Font = new Font("Century Gothic", 7.8F, FontStyle.Bold);
+            btnCadastrar.ForeColor = Color.White;
+            btnCadastrar.Location = new Point(27, 336);
+            btnCadastrar.Name = "btnCadastrar";
+            btnCadastrar.ShadowDecoration.CustomizableEdges = customizableEdges14;
+            btnCadastrar.Size = new Size(99, 37);
+            btnCadastrar.TabIndex = 0;
+            btnCadastrar.Text = "Cadastrar";
+            btnCadastrar.Click += btnCadastrar_Click;
+            // 
+            // lblMensagem
+            // 
+            lblMensagem.BackColor = Color.Transparent;
+            lblMensagem.Location = new Point(27, 309);
+            lblMensagem.Name = "lblMensagem";
+            lblMensagem.Size = new Size(61, 22);
+            lblMensagem.TabIndex = 3;
+            lblMensagem.Text = "Exemplo";
             // 
             // frmCadastroUsuario
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(310, 424);
+            ClientSize = new Size(310, 396);
+            Controls.Add(lblMensagem);
             Controls.Add(groupBox1);
             Controls.Add(guna2Panel1);
-            Controls.Add(guna2Button2);
-            Controls.Add(guna2Button1);
+            Controls.Add(btnCadastrar);
+            Controls.Add(btnCancelar);
             FormBorderStyle = FormBorderStyle.None;
             Name = "frmCadastroUsuario";
             Text = "frmCadastroUsuario";
+            Load += frmCadastroUsuario_Load;
             guna2Panel1.ResumeLayout(false);
             guna2Panel1.PerformLayout();
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private Guna.UI2.WinForms.Guna2Button btnCancelar;
         private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
         private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
         private GroupBox groupBox1;
@@ -264,6 +279,7 @@
         private Guna.UI2.WinForms.Guna2TextBox txtLogin;
         private Guna.UI2.WinForms.Guna2TextBox txtNome;
         private Guna.UI2.WinForms.Guna2CheckBox chkAtivo;
-        private Guna.UI2.WinForms.Guna2Button guna2Button2;
+        private Guna.UI2.WinForms.Guna2Button btnCadastrar;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblMensagem;
     }
 }
--- CadastroClientes.UI/frmCadastroUsuario.cs
+++ CadastroClientes.UI/frmCadastroUsuario.cs
@@ -1,18 +1,104 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Text;
-using System.Windows.Forms;
+﻿using CadastroClientes.Application.Services;
 
-namespace CadastroClientes.UI
+namespace CadastroClientes.UI;
+
+public partial class frmCadastroUsuario : Form
 {
-    public partial class frmCadastroUsuario : Form
+    private readonly AutenticacaoService _autenticacaoService;
+
+    public string LoginCadastrado { get; private set; } = string.Empty;
+
+    public frmCadastroUsuario(AutenticacaoService autenticacaoService)
     {
-        public frmCadastroUsuario()
+        _autenticacaoService = autenticacaoService;
+        InitializeComponent();
+    }
+
+    private void frmCadastroUsuario_Load(object sender, EventArgs e)
+    {
+        txtNome.Focus();
+        lblMensagem.Text = string.Empty;
+    }
+
+    private async void btnCadastrar_Click(object sender, EventArgs e)
+    {
+        if (string.IsNullOrWhiteSpace(txtNome.Text))
         {
-            InitializeComponent();
+            MostrarErro("O campo Nome é obrigatório.");
         }
+
+        if (string.IsNullOrWhiteSpace(txtLogin.Text))
+        {
+            MostrarErro("O campo Login é obrigatório.");
+        }
+
+        if (string.IsNullOrWhiteSpace(txtSenha.Text))
+        {
+            MostrarErro("O campo Senha é obrigatório.");
+        }
+
+        if (txtSenha.Text != txtConfirmarSenha.Text)
+        {
+            MostrarErro("As senhas não conferem. Tente novamente.");
+            txtConfirmarSenha.Clear();
+            txtConfirmarSenha.Focus();
+            return;
+        }
+
+        btnCadastrar.Enabled = false;
+        lblMensagem.ForeColor = Color.Green;
+        lblMensagem.Text = "Cadastrando...";
+
+        try
+        {
+            var (sucesso, mensagem) = await _autenticacaoService.RegistrarUsuarioAsync(
+                txtNome.Text.Trim(),
+                txtLogin.Text.Trim(),
+                txtSenha.Text
+                );
+
+            if (sucesso)
+            {
+                LoginCadastrado = txtLogin.Text.ToLower().Trim();
+
+                MessageBox.Show(
+                    $"Usuário: '{LoginCadastrado}' cadastrado com sucesso!\n\nAgora faça login com suas credenciais.",
+                    "Cadastro Realizado",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+
+                DialogResult = DialogResult.OK;
+                Close();
+            }
+            else
+            {
+                MostrarErro(mensagem);
+            }
+        }
+        catch (Exception ex)
+        {
+            MostrarErro("Erro ao cadastrar, verifique o banco de dados.");
+            MessageBox.Show($"Erro: {ex.Message}",
+                "Erro",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error);
+        }
+        finally
+        {
+            btnCadastrar.Enabled = true;
+        }
+
+    }
+
+    private void MostrarErro(string mensagem)
+    {
+        lblMensagem.ForeColor = Color.Red;
+        lblMensagem.Text = mensagem;
+    }
+
+    private void btnCancelar_Click(object sender, EventArgs e)
+    {
+        Close();
     }
 }
+
